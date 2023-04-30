c5cc08f3eb4d251ee2abf19a9e74d6f6149afd09
Vitor Nogueira <vslnogueira96@gmail.com>
2023-04-30 11:39:09 -0300

    feat: criando endpoint /users/id e criando função findById para retornar um usuário baseado no id do param

--- src/main/java/com/primeiraapijava/demo/controllers/UserController.java
+++ src/main/java/com/primeiraapijava/demo/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.primeiraapijava.demo.entities.User;
 import com.primeiraapijava.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,9 @@ public class UserController {
     @GetMapping
     public List<User> findAll() {
         return repository.findAll();
+    }
+    @GetMapping (value = "/{id}")
+    public User findById(@PathVariable Long id) {
+        return repository.findById(id).get();
     }
 }
