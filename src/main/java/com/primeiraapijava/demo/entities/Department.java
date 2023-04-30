0c7325412e73f373a2c7eda925e7631f25289992
Vitor Nogueira <vslnogueira96@gmail.com>
2023-04-30 11:13:51 -0300

    feat: mapeando tabela department com user

--- src/main/java/com/primeiraapijava/demo/entities/Department.java
+++ src/main/java/com/primeiraapijava/demo/entities/Department.java
@@ -1,6 +1,13 @@
 package com.primeiraapijava.demo.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_department")
 public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
 
